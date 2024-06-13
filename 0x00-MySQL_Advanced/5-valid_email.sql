136c95109a6d1f9690b953d036c0e499fdb282ff
Mrelshimy <mraafat.elsayed@gmail.com>
2024-06-14 02:10:34 +0300

    bugfix

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -7,7 +7,7 @@ CREATE TRIGGER changed_email
 AFTER UPDATE ON users
 FOR EACH ROW
 BEGIN
-    IF OLD.email!= NEW.email THEN
+    IF OLD.email != NEW.email THEN
         UPDATE users SET valid_email = 0;
     END IF;
 END$$
