9ca39068ff22d03aa559566606f070669e583b4b
Mrelshimy <mraafat.elsayed@gmail.com>
2024-06-14 02:09:11 +0300

    task5

---
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,17 @@
+-- SQL query to create a trigger
+
+
+DELIMITER $$
+
+CREATE TRIGGER changed_email
+AFTER UPDATE ON users
+FOR EACH ROW
+BEGIN
+    IF OLD.email!= NEW.email THEN
+        UPDATE users SET valid_email = 0 WHERE id = NEW.id;
+    END IF;
+END$$
+
+DELIMITER ;
+
+    
