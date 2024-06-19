f796078c7dc5e06a1934fb36b00b8f4e4314bcff
Mrelshimy <mraafat.elsayed@gmail.com>
2024-06-19 13:25:36 +0300

    task1

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,7 +2,7 @@
 """ Redis Operations in python """
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable, Optional
 
 
 class Cache:
@@ -18,3 +18,21 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Optional[Callable])\
+            -> Union[str, bytes, int, float]:
+        """ Return Key value to original format using fn """
+        value = self._redis.get(key)
+        if fn:
+            return fn(value)
+        return value
+
+    def get_str(self, value: str) -> str:
+        """ Function to return int value of value"""
+        return value.decode('utf-8')
+
+    def get_int(self, value: str) -> int:
+        """ Function to return string value of value"""
+        if value.decode('utf-8'):
+            return int(value.decode('utf-8'))
+        return 0
